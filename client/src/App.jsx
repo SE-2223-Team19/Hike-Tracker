a2dce25e42bdb4f0dc9a0d7e7c6f53cd6b4d621f
Victor3099 <vittorio.arpino@studenti.polito.it>
2022-11-07 12:34:30 +0100

    Adding setup for client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,39 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import API from './api';
+
+
 function App() {
-	return <h1>Hike tracker</h1>;
+
+	
+	/*
+	Example
+
+	return (<BrowserRouter>
+		<Routes>
+		  <Route path="/" element={<Layout mode="login"/>} />
+		</Routes>
+	  </BrowserRouter>
+	);
+	*/
+}
+
+function Layout(props) {
+
+	// mode = props.mode;
+
+	/*
+
+	Example
+
+	let outlet = <></>;
+	if(mode === "login") {
+		outlet = <LoginPage/>;
+	}
+
+	// Page returned to the browser on considering the mode specified on the routes in App component
+	return outlet;	
+
+	*/
 }
 
 export default App;

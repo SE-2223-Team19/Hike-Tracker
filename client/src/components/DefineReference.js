1ac81da9c3bf10840912adea06643a068f84f165
Pouya Hakimi Fard S296734 <S296734@studenti.polito.it>
2022-11-24 18:57:04 +0100

    GUI

--- client/src/components/DefineReference.js
+++ client/src/components/DefineReference.js
@@ -11,32 +11,6 @@ async function DataLoad(){
 
 }
 
-// const editreference = (description) => {
-//   console.log(description);
-//   const edit= (des)=>(des.map((e)=>(e.Title==description.Title ? description : e)))
-//   console.log(edit);
-//   //setreference(edit);
-//   }
-
-
-function DefineReferenceTable() {
-  return (
-    <Table striped>
-      <thead>
-        <tr>
-          <th>Location Type</th>
-          <th>Latitude Point</th>
-          <th>Longitude Point</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-      <tbody>
-       
-     
-      </tbody>
-    </Table>
-  );
-}
 
 
 
@@ -61,14 +35,17 @@ function DefineReferenceForm(){
   
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Latitude Point</Form.Label>
-          <Form.Control type="text" placeholder="Insert Latitude" />
+          <Form.Select>
+          <option></option>  
+          
+        </Form.Select>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Longitude Point</Form.Label>
-          <Form.Control type="text" placeholder="Insert Longitude" />
+          <Form.Select><option></option></Form.Select>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
         </Form.Group>
@@ -90,63 +67,11 @@ function DefineReferenceForm(){
 
 
 
-// function DescriptionRow(props){
-    
-
-//     return<tr>
-//        {/* <td className="align-middle p5555">{props.Description.title}</td> */}
-  
-//         <Description Description={props.Description.Description}/>
-//         <StartPoint Description={props.Description.StartPoint}/>
-//         <EndPoint Description={props.Description.EndPoint}/>
-//         <ReferencePoints Description={props.Description.ReferencePoints}/>
-//         <ActionButtons Description={props.Description} RemoveDescriprtion={props.RemoveDescriprtion} setFormstatus={props.setFormstatus} editDescription={props.editDescription} setFormMode={props.setFormMode} formMode={props.formMode} setEditedDescription={props.setEditedDescription}/>
-
-//     </tr>
-// }
-
-
-
-// function Title(props){
-//     return<>
-//   <td className="align-middle">{props.Description}</td>
-//  </>
-// }
-
-// function Description(props){
-//     return<>
-//   <td className="align-middle">{props.Description}</td>
-//  </>
-// }
-// function StartPoint(props){
-//     return<>
-//   <td className="align-middle">{props.Description}</td>
-//  </>
-// }
-// function EndPoint(props){
-//     return<>
-//   <td className="align-middle">{props.Description}</td>
-//  </>
-// }
-// function ReferencePoints(props){
-//     return<>
-//   <td className="align-middle">{props.Description}</td>
-//  </>
-// }
-// function ActionButtons(props) {
-  
-  
-//     return (  
-//        <td> <Button className="rounded-circle" disabled={props.formMode=='edit'} variant="outline-warning" onClick={()=>{props.setFormMode('edit');props.setEditedDescription(props.Description)}}>Edit</Button>{' '}
-//         <Button className="rounded-circle" disabled={props.formMode!==''} variant="outline-danger" onClick={()=>{props.RemoveDescriprtion(props.Description.Title)}}>Delete</Button>{' '}
-        
-//         </td>
-      
-//     )
-//   }
 
 
 
 
-const Reference={DefineReferenceForm, DefineReferenceTable}
+
+
+const Reference={DefineReferenceForm}
 export default Reference
--- client/src/pages/DefineReference.jsx
+++ client/src/pages/DefineReference.jsx
@@ -6,15 +6,13 @@ function DefineReferencepage() {
 	return (
 		<>
             <Container>
-            <Card>
-      <Card.Body><Header/></Card.Body>
-             </Card>
+            
+             <Header/>
+             
              <Table>
                 <Row><h1 className="md-5">Define Reference Point</h1></Row>
              </Table>
-            <Table>
-               <Row><Reference.DefineReferenceTable/></Row>
-            </Table>
+            
             <Table>
                 
             </Table>

7cf33ec434fd9756d91141057be4dad229c454c8
Pouya Hakimi Fard S296734 <S296734@studenti.polito.it>
2022-11-24 17:14:04 +0100

    test

--- client/src/components/DefineReference.js
+++ client/src/components/DefineReference.js
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import {Form,Button,Row,Col} from "react-bootstrap"
-
 import Table from 'react-bootstrap/Table';
+import { getHikes } from "../api/hikes";
+
+//const [reference,setreference]=useState("")
+
+async function DataLoad(){
+  
+  let result =await getHikes()
+
+}
+
+// const editreference = (description) => {
+//   console.log(description);
+//   const edit= (des)=>(des.map((e)=>(e.Title==description.Title ? description : e)))
+//   console.log(edit);
+//   //setreference(edit);
+//   }
+
 
 function DefineReferenceTable() {
   return (
@@ -14,12 +31,7 @@ function DefineReferenceTable() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
+       
      
       </tbody>
     </Table>
@@ -73,6 +85,67 @@ function DefineReferenceForm(){
       </Form>
     )
 }
+
+
+
+
+
+// function DescriptionRow(props){
+    
+
+//     return<tr>
+//        {/* <td className="align-middle p5555">{props.Description.title}</td> */}
+  
+//         <Description Description={props.Description.Description}/>
+//         <StartPoint Description={props.Description.StartPoint}/>
+//         <EndPoint Description={props.Description.EndPoint}/>
+//         <ReferencePoints Description={props.Description.ReferencePoints}/>
+//         <ActionButtons Description={props.Description} RemoveDescriprtion={props.RemoveDescriprtion} setFormstatus={props.setFormstatus} editDescription={props.editDescription} setFormMode={props.setFormMode} formMode={props.formMode} setEditedDescription={props.setEditedDescription}/>
+
+//     </tr>
+// }
+
+
+
+// function Title(props){
+//     return<>
+//   <td className="align-middle">{props.Description}</td>
+//  </>
+// }
+
+// function Description(props){
+//     return<>
+//   <td className="align-middle">{props.Description}</td>
+//  </>
+// }
+// function StartPoint(props){
+//     return<>
+//   <td className="align-middle">{props.Description}</td>
+//  </>
+// }
+// function EndPoint(props){
+//     return<>
+//   <td className="align-middle">{props.Description}</td>
+//  </>
+// }
+// function ReferencePoints(props){
+//     return<>
+//   <td className="align-middle">{props.Description}</td>
+//  </>
+// }
+// function ActionButtons(props) {
+  
+  
+//     return (  
+//        <td> <Button className="rounded-circle" disabled={props.formMode=='edit'} variant="outline-warning" onClick={()=>{props.setFormMode('edit');props.setEditedDescription(props.Description)}}>Edit</Button>{' '}
+//         <Button className="rounded-circle" disabled={props.formMode!==''} variant="outline-danger" onClick={()=>{props.RemoveDescriprtion(props.Description.Title)}}>Delete</Button>{' '}
+        
+//         </td>
+      
+//     )
+//   }
+
+
 
 
 const Reference={DefineReferenceForm, DefineReferenceTable}

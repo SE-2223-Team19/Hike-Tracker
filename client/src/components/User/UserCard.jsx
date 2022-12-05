8ed03c10aa765aa04da02222db0203b02e6a962d
matteorosani <matteo.rosani@gmail.com>
2022-12-05 07:59:07 +0100

    Updated data Added verified email tooltip and icon

--- client/src/components/User/UserCard.jsx
+++ client/src/components/User/UserCard.jsx
@@ -1,12 +1,21 @@
-import { React, useState } from "react";
-import { Card, Form, Stack, Badge } from "react-bootstrap";
+import { React, useState, forwardRef } from "react";
+import { Card, Form, Stack, Badge, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { updateUser } from "../../api/users";
 import { capitalizeAndReplaceUnderscores, userTypeToColour } from "../../helper/utils";
+import { CgCheckO } from "react-icons/cg";
+
+const EmailVerifiedIcon = forwardRef((props, ref) => {
+    return (
+        <span ref={ref}>
+            <CgCheckO {...props} />
+        </span>
+    );
+});
 
 function UserRow({user, setDirty}) {
 
     const [updating, setUpdating] = useState(false);
-    
+
     return (
         <Card className="flex-row p-3 mt-4">
 			<Card.Body>
@@ -20,6 +29,19 @@ function UserRow({user, setDirty}) {
                 </Card.Title>
                 <Stack direction="horizontal">
                     <div className="d-flex flex-row">
+                        {
+                            user.isEmailValidated &&
+                            <OverlayTrigger
+                                placement="top"
+                                overlay={
+                                    <Tooltip>
+                                        The email is verified
+                                    </Tooltip>
+                                }
+                            >
+                                <EmailVerifiedIcon style={{"color": "rgba(var(--bs-success-rgb)"}} />
+                            </OverlayTrigger>
+                        }
                         <span className="ms-1">{user.email}</span>
                     </div>
                     <div className="d-flex flex-row ms-auto">

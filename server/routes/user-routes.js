65c7eb17e6eb7732f3d94fd9d633dfa0d23dbebb
Lorenzo Pantano <l.pantano97@gmail.com>
2022-12-05 12:29:37 +0100

    TD Refactoring

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -2,11 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const userController = require("../controllers/user-controller");
 
-// userRouter.get("/", userController.getUsers);
-// userRouter.get("/:id", userController.getUser);
 userRouter.post("/", userController.createUser);
 userRouter.post("/verify/:uniqueString", userController.verifyUser);
-// userRouter.patch("/:id", userController.updateUser);
-// userRouter.delete("/:id", userController.deleteUser);
 
 module.exports = userRouter;

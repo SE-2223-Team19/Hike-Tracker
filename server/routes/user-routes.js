4ef62cca2e96be3772bc064e967de257d8278732
martacorci1 <62058633+martacorci1@users.noreply.github.com>
2022-12-09 21:09:59 +0100

    added auth middlewares to preferences

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const userRouter = express.Router();
 const userController = require("../controllers/user-controller");
+const { isHiker } = require("../authorization-middlewares");
 
 userRouter.post("/", userController.createUser);
 userRouter.post("/verify/:uniqueString", userController.verifyUser);
-userRouter.get("/preferences", userController.getPreferences);
-userRouter.patch("/preferences", userController.updatePreferences);
-userRouter.delete("/preferences", userController.deletePreferences);
+userRouter.get("/preferences", isHiker, userController.getPreferences);
+userRouter.patch("/preferences", isHiker, userController.updatePreferences);
+userRouter.delete("/preferences", isHiker, userController.deletePreferences);
 // userRouter.patch("/:id", userController.updateUser);
 // userRouter.delete("/:id", userController.deleteUser);
 

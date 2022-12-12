00babc00719430f6e2d794d8cff4ebcc9cabd8af
Lorenzo Pantano <l.pantano97@gmail.com>
2022-12-12 11:41:00 +0100

    frontend hike card with image

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -6,12 +6,9 @@ const { isHiker } = require("../authorization-middlewares");
 userRouter.post("/", userController.createUser);
 userRouter.post("/verify/:uniqueString", userController.verifyUser);
 userRouter.get("/", userController.getUsers);
-// userRouter.get("/:id", userController.getUser);
 userRouter.patch("/:id", userController.updateUser);
 userRouter.get("/preferences", isHiker, userController.getPreferences);
 userRouter.patch("/preferences", isHiker, userController.updatePreferences);
 userRouter.delete("/preferences", isHiker, userController.deletePreferences);
-// userRouter.patch("/:id", userController.updateUser);
-// userRouter.delete("/:id", userController.deleteUser);
 
 module.exports = userRouter;

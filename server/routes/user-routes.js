ca4f51d96152f7ade4ef036afde3f6c43b6f44ec
martacorci1 <62058633+martacorci1@users.noreply.github.com>
2022-12-15 20:36:29 +0100

    small fix

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -6,9 +6,9 @@ const { isHiker } = require("../authorization-middlewares");
 userRouter.post("/", userController.createUser);
 userRouter.post("/verify/:uniqueString", userController.verifyUser);
 userRouter.get("/", userController.getUsers);
-userRouter.patch("/:id", userController.updateUser);
 userRouter.get("/preferences", isHiker, userController.getPreferences);
 userRouter.patch("/preferences", isHiker, userController.updatePreferences);
 userRouter.delete("/preferences", isHiker, userController.deletePreferences);
+userRouter.patch("/:id", userController.updateUser);
 
 module.exports = userRouter;
